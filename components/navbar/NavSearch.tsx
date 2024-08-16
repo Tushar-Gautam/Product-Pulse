24778b31e65e88d53e5d142d8b24e2f72396846e
Tushar Gautam <tushargautam@Tushars-MacBook-Air.local>
2024-08-16 16:29:21 +0530

    added search functionality

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,43 @@
+"use client";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+import { useState, useEffect } from "react";
 import { Input } from "../ui/input";
 
 function NavSearch() {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const trimmedValue = value.trim();
+    const params = new URLSearchParams(searchParams);
+    if (!value || !trimmedValue) {
+      params.delete("search");
+    } else {
+      params.set("search", trimmedValue);
+    }
+    replace(`/products?${params.toString()}`);
+  }, 300);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams]);
+
   return (
     <Input
       type="search"
-      placeholder="Search product..."
-      className="max-w-xs dark::bg-muted"
+      placeholder="search product..."
+      className="max-w-xs dark:bg-muted"
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={search}
     />
   );
 }
