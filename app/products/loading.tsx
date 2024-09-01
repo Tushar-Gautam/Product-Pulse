c740958c33ebabfdf78773323813431cd3247b4d
Tushar Gautam <tushargautam999@gmail.com>
2024-09-01 14:55:47 +0530

    fixed loader

--- app/products/loading.tsx
+++ app/products/loading.tsx
@@ -3,8 +3,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 function loading() {
   return (
-    <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
-      <Skeleton className="h-[33vw]" />
+    <div className="pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <Skeleton className="h-64" />
+      <Skeleton className="h-64" />
+      <Skeleton className="h-64" />
     </div>
   );
 }
