22fc703a1222f6bf02d11caabdda1f40f80b5e54
Tushar Gautam <tushargautam999@gmail.com>
2024-09-01 15:01:36 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/navbar/Navbar";
 import Container from "@/components/global/Container";
 import Providers from "./Providers";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
             <Navbar />
             <Container className="py-20">{children}</Container>
           </Providers>
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
